28b9bddd1b2004cf017c5065b61090e163163f51
MAFLothers <Moohamedali571@gmail.com>
2026-05-11 20:49:07 +0300

    Generate HTML for the order summary at checkout

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,14 @@
 // This file will hold the cart items, which will be added to when the user clicks "Add to Cart" on a product. It will be an array of objects, where each object represents a product that has been added to the cart. Each object will have the following properties: id, name, price, quantity, and image.
 //export is used to make the cart variable available to other files, such as amazon.js, so that we can manipulate the cart from there.
 export const cart = [
-    
+    {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },
+    {
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }
 ];
 export function addToCart(productId){
  let matchingItem;
