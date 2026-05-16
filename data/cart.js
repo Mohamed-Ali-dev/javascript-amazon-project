965c0a2026a4c95c83441d3099e5380b0e606409
Flothers1 <ifreeze@flothers.com>
2026-05-17 02:10:39 +0300

    Using local storage at cart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,24 @@
 // This file will hold the cart items, which will be added to when the user clicks "Add to Cart" on a product. It will be an array of objects, where each object represents a product that has been added to the cart. Each object will have the following properties: id, name, price, quantity, and image.
 //export is used to make the cart variable available to other files, such as amazon.js, so that we can manipulate the cart from there.
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+// add the productId and quantity for a product
+//the Id is 
 export function addToCart(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
@@ -25,6 +34,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 // Delete product from cart we get the id from the checkout.js
 export function removeFromCart(productId) {
@@ -35,4 +45,5 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
 }
