785fb2654b5e135d8f57752d267ad2118861f475
Flothers1 <ifreeze@flothers.com>
2026-06-02 23:00:23 +0300

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -6,10 +6,12 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -18,7 +20,7 @@ function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 // add the productId and quantity for a product
-//the Id is 
+//the Id is
 export function addToCart(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
@@ -32,6 +34,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
